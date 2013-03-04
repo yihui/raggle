2df5b5d0a739d745975613da3205cc0c7d32b964
Yihui Xie <xie@yihui.name>
2013-03-04 15:40:35 -0600

    hide psy when the group number has been chosen

--- ui.R
+++ ui.R
@@ -13,7 +13,9 @@ shinyUI(pageWithSidebar(
     helpText('The leader board shows the prediction error rates. Group numbers
              are in the first column. NA means no attempt has been made.',
              a(href='https://github.com/yihui/raggle#readme', 'More info here.')),
-    div(img(src = 'http://www.kiss925.com/files/365984-gangnam-style.jpg'))
+    conditionalPanel('input.group == 0',
+                     div(img(src = 'http://www.kiss925.com/files/365984-gangnam-style.jpg'))
+    )
   ),
 
   mainPanel(
